f8919dd32b8bf0c4574b6c2f7e0facdf706fa3ea
Sandro Mancuso <sandromancuso@yahoo.com>
2014-08-10 18:48:36 +0100

    Application is launched for journey tests.

--- src/main/java/com/codurance/Gandalf.java
+++ src/main/java/com/codurance/Gandalf.java
@@ -1,8 +1,13 @@
 package com.codurance;
 
+import com.codurance.infrastructure.GaldalfSpark;
+import com.codurance.infrastructure.Routes;
+
 public class Gandalf {
 
 	public void start() {
+		GaldalfSpark spark = new GaldalfSpark();
+		spark.init();
 	}
 
 }
