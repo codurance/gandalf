8d773d08a89963727db80081ae000b1257bdf384
Sandro Mancuso <sandromancuso@yahoo.com>
2014-08-13 22:47:12 +0100

    proposals.js refactoring

--- src/main/webapp/js/gandalf/proposals.js
+++ src/main/webapp/js/gandalf/proposals.js
@@ -1,48 +1,33 @@
 var App = angular.module('App', [])
 
 App.controller('ProposalsController', function($scope, $http) {
+	var proposalsCallback = function(response) { $scope.proposals = response; };
 
-	$scope.proposals = [];
-
-	$scope.loadProposals = function() {
-		$http.get('/proposals/all')
-				.success(function(response){
-					$scope.proposals = response;
-				})
-				.error(function(response){
-					alert('Error: ' + response);
-				});
-	}
+	$scope.loadProposals = loadJsonData($http, '/proposals/all', proposalsCallback);
 });
 
 App.controller('NewProposalController', function($scope, $http) {
+	var clientsCallback = function(response){ $scope.clients = response; };
 
-	$scope.clients = [];
-
-	$scope.loadFormData = function() {
-		$http.get('/clients/all')
-				.success(function(response){
-					$scope.clients = response;
-				})
-				.error(function(response){
-					alert('Error: ' + response);
-				});
-	};
+	$scope.loadFormData = loadJsonData($http, '/clients/all', clientsCallback);
 });
 
 
 App.controller('ProposalController', function ($http, $scope) {
-
-	$scope.proposal = {};
+	var proposalCallback = function(response){ $scope.proposal = response; };
 
 	$scope.init = function(proposalId) {
 		var url = '/proposals/proposal/' + proposalId + '/json';
-		$http.get(url)
-				.success(function(response){
-					$scope.proposal = response;
-				})
-				.error(function(response){
-					alert('Error: ' + response);
-				});
+		loadJsonData($http, url, proposalCallback);
 	};
 });
+
+loadJsonData = function($http, url, successCallback) {
+	$http.get(url)
+			.success(successCallback)
+			.error(displayError);
+}
+
+displayError = function(message) {
+	alert('Error: ' + message);
+}
