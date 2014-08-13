2532b21240108f29248bfaadb52d0ad3b5635f30
Sandro Mancuso <sandromancuso@yahoo.com>
2014-08-13 23:05:06 +0100

    Isolating proposal AngularJS module.

--- src/main/webapp/js/gandalf/proposals.js
+++ src/main/webapp/js/gandalf/proposals.js
@@ -1,33 +1,37 @@
-var App = angular.module('App', [])
+(function() {
 
-App.controller('ProposalsController', function($scope, $http) {
-	var proposalsCallback = function(response) { $scope.proposals = response; };
+	var App = angular.module('ProposalApp', [])
 
-	$scope.loadProposals = loadJsonData($http, '/proposals/all', proposalsCallback);
-});
+	App.controller('ProposalsController', function($scope, $http) {
+		var proposalsCallback = function(response) { $scope.proposals = response; };
 
-App.controller('NewProposalController', function($scope, $http) {
-	var clientsCallback = function(response){ $scope.clients = response; };
+		$scope.loadProposals = loadJsonData($http, '/proposals/all', proposalsCallback);
+	});
 
-	$scope.loadFormData = loadJsonData($http, '/clients/all', clientsCallback);
-});
+	App.controller('NewProposalController', function($scope, $http) {
+		var clientsCallback = function(response){ $scope.clients = response; };
+
+		$scope.loadFormData = loadJsonData($http, '/clients/all', clientsCallback);
+	});
 
 
-App.controller('ProposalController', function ($http, $scope) {
-	var proposalCallback = function(response){ $scope.proposal = response; };
+	App.controller('ProposalController', function ($http, $scope) {
+		var proposalCallback = function(response){ $scope.proposal = response; };
 
-	$scope.init = function(proposalId) {
-		var url = '/proposals/proposal/' + proposalId + '/json';
-		loadJsonData($http, url, proposalCallback);
-	};
-});
+		$scope.init = function(proposalId) {
+			var url = '/proposals/proposal/' + proposalId + '/json';
+			loadJsonData($http, url, proposalCallback);
+		};
+	});
 
-loadJsonData = function($http, url, successCallback) {
-	$http.get(url)
-			.success(successCallback)
-			.error(displayError);
-}
+	loadJsonData = function($http, url, successCallback) {
+		$http.get(url)
+				.success(successCallback)
+				.error(displayError);
+	}
 
-displayError = function(message) {
-	alert('Error: ' + message);
-}
+	displayError = function(message) {
+		alert('Error: ' + message);
+	}
+
+})();
