5bd6450011c2790746bcd25cb86952cd97766d8b
Sandro Mancuso <sandromancuso@yahoo.com>
2014-08-20 21:56:44 +0100

    GandalfSpark removed. All its logic was moved to Gandalf.

--- src/main/java/com/codurance/Gandalf.java
+++ src/main/java/com/codurance/Gandalf.java
@@ -1,12 +1,39 @@
 package com.codurance;
 
-import com.codurance.infrastructure.GaldalfSpark;
+import com.codurance.controllers.MainController;
+import com.codurance.controllers.ProposalController;
+import com.codurance.infrastructure.Controllers;
+import com.codurance.infrastructure.Routes;
+import com.noodlesandwich.rekord.Rekord;
+import spark.servlet.SparkApplication;
 
-public class Gandalf {
+/*
+    There are 2 ways to run Gandalf:
+
+    1. Executing the GandalfLauncher, which will start Jetty. Jetty will look for
+    a web.xml and will find this class (Gandalf) configured as a filter. Jetty will
+    run this class and that will trigger the SparkJava framework.
+
+    2. From Maven: mvn jetty:run. This will have a similar behaviour. Maven will
+    run Jetty that will look for a web.xml where this class is configured as a filter.
+ */
+public class Gandalf implements SparkApplication {
 
 	public void start() {
-		GaldalfSpark spark = new GaldalfSpark();
-		spark.init();
+		init();
 	}
 
+	@Override
+	public void init() {
+		MainController mainController = new MainController();
+		ProposalController proposalController = new ProposalController();
+
+		Rekord<Controllers> controllers = Controllers.rekord
+				.with(Controllers.mainController, mainController)
+				.with(Controllers.proposalController, proposalController);
+
+		Routes routes = new Routes(controllers);
+
+		routes.initialise();
+	}
 }
