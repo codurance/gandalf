246c7ff239d03f23054ec8f1f0128f16342ef539
Sandro Mancuso <sandromancuso@yahoo.com>
2014-08-21 11:04:25 +0100

    Stabilisation: Unit tests for ProposalController

--- src/test/java/main/com/codurance/controllers/ProposalControllerShould.java
+++ src/test/java/main/com/codurance/controllers/ProposalControllerShould.java
@@ -1,6 +1,9 @@
 package main.com.codurance.controllers;
 
 import com.codurance.controllers.ProposalController;
+import com.codurance.model.proposal.ProposalId;
+import com.codurance.view.NewProposalPage;
+import com.codurance.view.ProposalPage;
 import com.codurance.view.ProposalsPage;
 import org.junit.Before;
 import org.junit.Test;
@@ -18,6 +21,9 @@ import static org.mockito.BDDMockito.given;
 public class ProposalControllerShould {
 
 	private static final String PROPOSALS_PAGE = "proposals page";
+	private static final String PROPOSAL_PAGE = "proposal page";
+	private static final String NEW_PROPOSAL_PAGE = "new proposal page";
+
 	private ProposalsPage proposalsPage = new ProposalsPage();
 
 	@Mock Request request;
@@ -39,6 +45,29 @@ public class ProposalControllerShould {
 		String page = proposalController.displayProposals(request, response);
 
 		assertThat(page, is(PROPOSALS_PAGE));
+	}
+
+	@Test public void
+	display_proposal_for_proposal_id() {
+		ProposalPage proposalPage = new ProposalPage(new ProposalId("10"));
+		given(request.params(":proposalId")).willReturn("10");
+		given(templateRenderer.render(proposalPage.template(), proposalPage.model()))
+				.willReturn(PROPOSAL_PAGE);
+
+		String page = proposalController.displayProposalForProposalId(request, response);
+
+		assertThat(page, is(PROPOSAL_PAGE));
+	}
+
+	@Test public void
+	display_new_proposal_page() {
+		NewProposalPage newProposalPage = new NewProposalPage();
+		given(templateRenderer.render(newProposalPage.template(), newProposalPage.model()))
+				.willReturn(NEW_PROPOSAL_PAGE);
+
+		String page = proposalController.displayNewProposalPage(request, response);
+
+		assertThat(page, is(NEW_PROPOSAL_PAGE));
 	}
 
 }
