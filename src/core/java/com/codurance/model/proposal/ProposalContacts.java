ccbcf78beb95bd1092b1a5f8414c93e693040677
Sandro Mancuso <sandromancuso@yahoo.com>
2014-09-13 01:06:28 +0100

    ProposalContacts refactoring

--- src/core/java/com/codurance/model/proposal/ProposalContacts.java
+++ src/core/java/com/codurance/model/proposal/ProposalContacts.java
@@ -5,40 +5,39 @@ import com.eclipsesource.json.JsonObject;
 import com.eclipsesource.json.JsonValue;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
-import static java.util.Arrays.stream;
+import static java.util.Arrays.asList;
 import static org.apache.commons.lang3.builder.EqualsBuilder.reflectionEquals;
 import static org.apache.commons.lang3.builder.HashCodeBuilder.reflectionHashCode;
 
 public class ProposalContacts {
 
-	private Contact[] contacts;
+	private List<Contact> contacts = new ArrayList<>();
 
 	public ProposalContacts() {
 		this(new Contact[] {});
 	}
 
 	public ProposalContacts(Contact[] contacts) {
-		this.contacts = (contacts != null) ? contacts : new Contact[] {};
+		if (contacts != null) {
+			this.contacts = asList(contacts);
+		}
 	}
 
 	public static ProposalContacts from(ProposalJson proposalJson) {
 		List<Contact> contacts = new ArrayList<>();
 		Iterator<JsonValue> jsonContacts = proposalJson.getArray("contacts").iterator();
-		while (jsonContacts.hasNext()) {
-			JsonObject jsonContact = jsonContacts.next().asObject();
-			contacts.add(new Contact(jsonContact.get("name").asString(),
-					jsonContact.get("email").asString()));
-		}
+		jsonContacts.forEachRemaining((JsonValue jsonContact) ->
+				contacts.add(new Contact(jsonContact.asObject().get("name").asString(),
+										 jsonContact.asObject().get("email").asString())));
 		return new ProposalContacts(contacts.toArray(new Contact[contacts.size()]));
 	}
 
 	public JsonArray json() {
 		JsonArray contactsJson = new JsonArray();
-		stream(contacts).forEach(contact -> addContactJson(contactsJson, contact));
+		contacts.forEach(contact -> addContactJson(contactsJson, contact));
 		return contactsJson;
 	}
 
@@ -50,7 +49,7 @@ public class ProposalContacts {
 
 	@Override
 	public String toString() {
-		return Arrays.toString(contacts);
+		return contacts.toString();
 	}
 
 	@Override
