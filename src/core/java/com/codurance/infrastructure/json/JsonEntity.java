7ddeca0b4920667cb47b2729cdfdbcf969194870
Sandro Mancuso <sandromancuso@yahoo.com>
2014-09-25 01:33:30 +0100

    JsonEntity created and inherited by ProposalJson

---
+++ src/core/java/com/codurance/infrastructure/json/JsonEntity.java
@@ -0,0 +1,71 @@
+package com.codurance.infrastructure.json;
+
+import com.eclipsesource.json.JsonArray;
+import com.eclipsesource.json.JsonObject;
+import com.eclipsesource.json.JsonValue;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.eclipsesource.json.JsonObject.readFrom;
+import static java.time.LocalDate.parse;
+import static org.apache.commons.lang3.builder.EqualsBuilder.reflectionEquals;
+import static org.apache.commons.lang3.builder.HashCodeBuilder.reflectionHashCode;
+
+public class JsonEntity {
+
+	private final JsonObject jsonObject;
+
+	public JsonEntity() {
+		this(new JsonObject());
+	}
+
+	public JsonEntity(JsonObject jsonObject) {
+		this.jsonObject = jsonObject;
+	}
+
+	public JsonValue get(String name) {
+		return jsonObject.get(name);
+	}
+
+	public String getStringOrElse(String name, String elseValue) {
+		JsonValue value = get(name);
+		return (value != null) ? value.asString() : elseValue;
+	}
+
+	public LocalDate getDateOrElse(String name, DateTimeFormatter dateTimeFormat, LocalDate elseValue) {
+		JsonValue value = get(name);
+		return (value != null) ? parse(value.asString(), dateTimeFormat) : elseValue;
+	}
+
+	protected JsonObject newJsonObject() {
+		return readFrom(jsonObject.toString());
+	}
+
+	public JsonArray getArray(String name) {
+		JsonValue jsonValue = jsonObject.get(name);
+		return (jsonValue != null) ? jsonValue.asArray() : new JsonArray();
+	}
+
+	public JsonObject jsonObject() {
+		return this.jsonObject;
+	}
+
+	@Override
+	public String toString() {
+		return "ProposalJson{" +
+				"jsonObject=" + jsonObject.toString() +
+				'}';
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		return reflectionEquals(this, obj);
+	}
+
+	@Override
+	public int hashCode() {
+		return reflectionHashCode(this);
+	}
+
+}
--- src/core/java/com/codurance/model/proposal/ProposalJson.java
+++ src/core/java/com/codurance/model/proposal/ProposalJson.java
@@ -1,26 +1,17 @@
 package com.codurance.model.proposal;
 
+import com.codurance.infrastructure.json.JsonEntity;
 import com.eclipsesource.json.JsonArray;
 import com.eclipsesource.json.JsonObject;
-import com.eclipsesource.json.JsonValue;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
-import static com.eclipsesource.json.JsonObject.readFrom;
-import static java.time.LocalDate.parse;
-import static org.apache.commons.lang3.builder.EqualsBuilder.reflectionEquals;
-import static org.apache.commons.lang3.builder.HashCodeBuilder.reflectionHashCode;
-
-public class ProposalJson {
-	private JsonObject jsonObject;
+public class ProposalJson extends JsonEntity {
 
 	public ProposalJson() {
-		this.jsonObject = new JsonObject();
+		super();
 	}
 
 	public ProposalJson(JsonObject jsonObject) {
-		this.jsonObject = jsonObject;
+		super(jsonObject);
 	}
 
 	public ProposalJson add(String name, String value) {
@@ -35,56 +26,12 @@ public class ProposalJson {
 		return new ProposalJson(newJsonObject().add(name, jsonArray));
 	}
 
-	public JsonValue get(String name) {
-		return jsonObject.get(name);
-	}
-
-	public String getStringOrElse(String name, String elseValue) {
-		JsonValue value = get(name);
-		return (value != null) ? value.asString() : elseValue;
-	}
-
-	public LocalDate getDateOrElse(String name, DateTimeFormatter dateTimeFormat, LocalDate elseValue) {
-		JsonValue value = get(name);
-		return (value != null) ? parse(value.asString(), dateTimeFormat) : elseValue;
-	}
-
 	public ProposalJson set(String name, String value) {
 		return new ProposalJson(newJsonObject().set(name, value));
 	}
 
 	public ProposalJson set(String name, int value) {
 		return new ProposalJson(newJsonObject().set(name, value));
-	}
-
-	private JsonObject newJsonObject() {
-		return readFrom(jsonObject.toString());
-	}
-
-	public JsonArray getArray(String name) {
-		JsonValue jsonValue = jsonObject.get(name);
-		return (jsonValue != null) ? jsonValue.asArray() : new JsonArray();
-	}
-
-	public JsonObject jsonObject() {
-		return this.jsonObject;
-	}
-
-	@Override
-	public String toString() {
-		return "ProposalJson{" +
-				"jsonObject=" + jsonObject.toString() +
-				'}';
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		return reflectionEquals(this, obj);
-	}
-
-	@Override
-	public int hashCode() {
-		return reflectionHashCode(this);
 	}
 
 }
